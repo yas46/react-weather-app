f5f3d8e09c96cd0af72f3d56af476c703533f2db
Yasser Beyer <yas2pt0@gmail.com>
2018-03-10 12:20:27 -0500

    structure

---
+++ src/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './Logo/netelogo.svg';
+// import './header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>header</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/weather.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './weather.css';
+
+class Weather extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>weather</h1>
+      </div>
+    );
+  }
+}
+
+export default Weather;
