0a3b49457202a0e68caee3fd68f3c373e609d3a7
Yasser Beyer <yas2pt0@gmail.com>
2018-03-10 12:36:31 -0500

    location component

---
+++ src/location.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './location.css';
+
+class Location extends Component {
+  render() {
+    return (
+      <div className="location">
+        <h1>location</h1>
+      </div>
+    );
+  }
+}
+
+export default Location;
